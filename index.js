4d1201018043053ca7af6723f9ceab310a716991
Jesse <jessyvee1@gmail.com>
2023-03-19 18:07:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let name =["Guadalupe","Ollie","Aki"]
+function writeCards(name,surprise){
+let messages = [];
+for (let i=0;i<name.length;i++){
+    let messages =["Thank you,${name[i]},for the wonderful messages.push(messages)"];
+}
+    return messages;
+}
+    writeCards(["Guadalupe","Ollie","Aki"],"surprise");
+    function countDown(){
+        let countDown = 0;
+        while(countDown<11){
+            console.log(countDown++)
+        }
+    }
